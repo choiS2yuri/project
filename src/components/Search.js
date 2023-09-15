e55cf5b6f954c4ad2b6e3a76c70d990be2ea8dfb
최유리 <choiyr5256@gmail.com>
2023-09-15 14:17:45 +0900

    0915

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,11 @@ import {ko} from 'date-fns/esm/locale'
 import {addDays, subDays} from 'date-fns'
 import { NavLink, useLocation } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
 
+const Wrap = styled.div`
+  height: 5000px;
+`
 
 function Search() {
   const [dateRange, setDateRange] = useState([null, null]);
@@ -15,8 +19,13 @@ function Search() {
   const [data, setData] = useState();
   const [alldata, setAllData] = useState();
   const [donm, setDonm] = useState("전체")
-
+  const [ischoice, setIsChoice] = useState([null,null]);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [page, setPage] = useState(1);
+  const [Selected, setSelected] = useState("");
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
 
     const updateScroll = () => {
         setScrollPosition(window.scrollY || document.documentElement.scrollTop);
@@ -25,30 +34,41 @@ function Search() {
     useEffect(() => {
         window.addEventListener("scroll", updateScroll);
     }, []);
-    useEffect(() =>{
-      axios.get(`https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=PROJECT&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json`).then(function(res){
-        // console.log(res.data.response.body.items.item);
-        setData(res.data.response.body.items.item);
-      })
-    },[])
-    // const [city, setCity] = useState(["모든 지역"])
-    // useEffect(()=>{
-    //   fetch("https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=PROJECT&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json").then((res) =>{
-    //     return res.json()
-    //   }).then((data)=> {
-    //     console.log(data)
-    //     // setCity(res.data.response.body.items.item)
-    //   });
-    // },[]);
-    
-    const FilterData = data && data.filter(e =>{
-      return donm === "전체" || donm === e.doNm
-     })
+    // useEffect(() =>{
+    //   axios.get(`https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=100&pageNo=${page}&MobileOS=ETC&MobileApp=PROJECT&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json`).then(function(res){
+    //     // console.log(res.data.response.body.items.item);
+    //     setData(res.data.response.body.items.item);
+    //   })
+    // },[])
 
+    
+    useEffect(()=>{
+      fetch("https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=100&pageNo=${page}&MobileOS=ETC&MobileApp=PROJECT&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json").then((res) =>{
+        return res.json()
+      }).then((res)=> {
+        // console.log(data)
+        setDonm(res.data.response.body.items.item)
+      }); 
+    },[]);
+    const [optiondonmSelect, setOptionDonmSelect] = useState("");
+    const optionDonm = (e) =>{
+        const donmValue = e.target.value
+        setOptionDonmSelect(donmValue);
+        
+        if(donmValue !== '모든 지역'){
+            setIsChoice([1,0])
+        }else{
+          setIsChoice([0,0])
+        } 
+      }
+    
+    // const FilterData = data && data.filter(e =>{
+    //   return donm === "전체" || donm === e.doNm
+    //  })  
 
   return (
     <>
-      <div className="h-[5000px]">
+      <Wrap>
       {/* 모바일 써치+버튼 시작 */}
         <div className={`z-50 bg-white pt-5 w-[100%] fixed top-0 flex justify-around ${scrollPosition > 500 ? "block" : "hidden"}`}>
           <div className=" max-w-[70%] h-9 flex justify-between flex-nowrap rounded-lg border items-center lg:basis-3/4 relative" >
@@ -64,20 +84,20 @@ function Search() {
             </div>
           </div>
         </div>
-      {/* 모바일 써치+버튼 끝 */}
-                
+      {/* 모바일 써치+버튼 끝 */}               
         {/* 유리써치 */}
         <div className="mt-5 w-full h-[300px]">
           <div className="max-w-7xl h-9 mx-auto flex justify-between flex-nowrap rounded-lg border items-center lg:basis-10/12" >
-            <select className=''>
+            {/* <select onChange={handleSelect} value={Selected}>
               <option>전체</option>
                 {
                   data && FilterData.map((e,i)=>{
                     return(
-                    <li key={i}>{e.doNm}</li>
+                    <li key={i} value={e.doNm}>{e.doNm}</li>
                     )
                   })
                 }
+            </select> */}
                 {/* <option value="0">전체</option>
                <option value="1">서울</option>
                <option value="2">부산시</option>
@@ -88,6 +108,12 @@ function Search() {
                <option value="7">울산시</option>
                <option value="8">세종시</option>
                <option value="9">경기도</option> */}
+            <select  onChange={optionDonm} className='text-[24px]  font-bold'>
+              <option value="전체" >전체</option>
+                {
+                  data.map((e,i) =>
+                  <option key={i} value={e.doNm}>{e.doNm}</option>)
+                }
             </select>
             <DatePicker
               locale={ko}
@@ -106,7 +132,7 @@ function Search() {
           </div>
         </div>
         {/* 유리써치 끝*/}
-      </div>
+      </Wrap>
     </>
   )
   }
